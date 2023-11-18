59f608402840c1618bead41c8c2f302441ceff1a
AlisaSviridenko <147500854+AlisaSviridenko@users.noreply.github.com>
2023-11-18 17:38:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-
+function check {
+	let action=document.getElementById("action").value;
+	let a=document.getElementById("number1").value;
+	let b=document.getElementById("number2").value;
+	let result=undefined;
+	switch (action){
+	case "add" result=Number (1)+Number (2); break
+	case "add" result=Number (1)-Number (2); break
+	case "add" result=Number (1)*Number (2); break
+	case "add" result=Number (1)/Number (2); break
+	default: result= Choose action";
+	}
+	let userAnswer=document.getElementById("result").value;
+if(userAnsver==rightAnsver){
+	alert ("OK")
+}
+else {
+	alert ("YOU ARE WRONG")
+}
+}
