580bebfe4200770075ea095c1ef6564f55a5058a
AlisaSviridenko <147500854+AlisaSviridenko@users.noreply.github.com>
2023-11-18 18:35:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function check {
+function check (){
 	let action=document.getElementById("action").value;
 	let a=document.getElementById("number1").value;
 	let b=document.getElementById("number2").value;
 	let result=undefined;
 	switch (action){
-	case "add" result=Number (1)+Number (2); break
-	case "substrate" result=Number (1)-Number (2); break
-	case "multiple" result=Number (1)*Number (2); break
-	case "divide" result=Number (1)/Number (2); break
+	case "add": result=Number (a)+Number (b); break
+	case "substrate": result=Number (a)-Number (b); break
+	case "multiple": result=Number (a)*Number (b); break
+	case "divide": result=Number (a)/Number (b); break
 	default: result= Choose action";
 	}
 	let userAnswer=document.getElementById("result").value;
@@ -17,4 +17,4 @@ if(userAnsver==rightAnsver){
 else {
 	alert ("YOU ARE WRONG")
 }
-}
+
