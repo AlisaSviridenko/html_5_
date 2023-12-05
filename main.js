1238283c86688c37ab2a8243bebb9c9151f08335
AlisaSviridenko <147500854+AlisaSviridenko@users.noreply.github.com>
2023-12-05 15:16:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -37,6 +37,13 @@ function check (){
         alert("YOU'RE WRONG");
         document.getElementById("nok").style.display=none;
     }
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+    
+let randomInt = getRandomInt(10, 20);
 
     // document.getElementById("result").value
 }
